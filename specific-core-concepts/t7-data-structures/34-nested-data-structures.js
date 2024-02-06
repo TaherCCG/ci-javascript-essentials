0d19d01001ada377a959c33731adcd860bc4d4ce
TaherCCG <taher.ccg@gmail.com>
2024-02-06 18:37:10 +0000

    add: Nested Data Structure challenge

---
+++ specific-core-concepts/t7-data-structures/34-nested-data-structures.js
@@ -0,0 +1,17 @@
+// Nested Data Structures
+// Nested data structures are data structures that contain other data structures.
+
+// javascript code
+let studentData = [
+    {
+    name: "John Smith",
+    email: "john@gmail.com",
+    subjects : ["Math", "Psychology", "Physics", "Chemistry","Biology"],
+}, {
+    name: "Mary Jones",
+    email: "mary@gmail.com",
+    subjects : ["Fine Art", "Music", "Biology", "Geography", "Politics"],
+}
+];
+
+console.log(studentData);
