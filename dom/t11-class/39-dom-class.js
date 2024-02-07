659ec95adf1a4fc4513cacc4c0b525e63e42e96f
TaherCCG <taher.ccg@gmail.com>
2024-02-07 16:11:01 +0000

    add:DOM Class

---
+++ dom/t11-class/39-dom-class.js
@@ -0,0 +1,5 @@
+let mainContent = document.getElementsByClassName('main-content');
+console.log(mainContent);
+
+let second = document.getElementsByClassName('section-content')[1];
+console.log(second);
