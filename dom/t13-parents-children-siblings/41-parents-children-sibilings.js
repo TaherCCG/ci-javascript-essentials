70297ce9bb7a947ee8cb411acb8228195b223955
TaherCCG <taher.ccg@gmail.com>
2024-02-07 16:12:06 +0000

    add:DOM Parents, Children & Siblings

---
+++ dom/t13-parents-children-siblings/41-parents-children-sibilings.js
@@ -0,0 +1,9 @@
+let parentElement = document.getElementsByClassName('section-content')[0].parentElement;
+
+console.log(parentElement);
+
+let children = document.getElementById('main').children;
+console.log(children);
+
+let sibling = document.getElementsByTagName('h1')[0].nextElementSibling;
+console.log(sibling);
