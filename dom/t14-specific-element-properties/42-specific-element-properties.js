a7c3cb899f141ae07d3a1c7088d738f33772dc6f
TaherCCG <taher.ccg@gmail.com>
2024-02-07 16:13:15 +0000

    add: DOM Specific Element Properties

---
+++ dom/t14-specific-element-properties/42-specific-element-properties.js
@@ -0,0 +1,9 @@
+let usernameInput = document.getElementById('username-input');
+let inputHtml = usernameInput.outerHTML;
+console.log(inputHtml);
+
+let inputPadding = (window.getComputedStyle(usernameInput).padding);
+
+
+console.log(inputPadding);
+
