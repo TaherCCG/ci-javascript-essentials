e26222a4c8891836a8b0fca9dad974c2f65e1bae
TaherCCG <taher.ccg@gmail.com>
2024-02-13 23:09:06 +0000

    Add:variables examples

---
+++ the-essentials/01-variables-primitive-data-types.js
@@ -0,0 +1,18 @@
+// Numbers
+
+let numInt = 10;
+console.log(numInt)
+let numFloat = 20.25;
+console.log(numFloat);
+
+
+// Strings
+
+let singleQuote ='A string using single quotes';
+console.log(singleQuote);
+let doubleQuote = "A string using double quotes" ;
+console.log(doubleQuote);
+
+
+
+
