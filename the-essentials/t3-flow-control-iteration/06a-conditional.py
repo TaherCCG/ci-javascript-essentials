49fa396493fc4d2ba4f732f2a98cb81f1290f611
TaherCCG <taher.ccg@gmail.com>
2024-02-16 22:39:35 +0000

    add: conditional py

---
+++ the-essentials/t3-flow-control-iteration/06a-conditional.py
@@ -0,0 +1,22 @@
+# Conditional statements
+
+color = "blue"
+primary = True
+
+
+second_statement = None 
+
+# write your if statement here
+if (color == 'blue'):
+    first_statement = "The color is blue"
+
+
+if(color == 'red' or primary):
+	second_statement = 'Either the color is red or primary is True'
+
+# Add your print statements here
+print(first_statement)
+print(second_statement)
+# Output: The color is blue
+# Output: Either the color is red or primary is True
+
