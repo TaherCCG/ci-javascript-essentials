9b0fc7743b36ccd9a7aeb9bfb7be0b9d1cdd2fe5
TaherCCG <taher.ccg@gmail.com>
2024-02-16 22:39:10 +0000

    add: Conditional js

---
+++ the-essentials/t3-flow-control-iteration/06-conditional.js
@@ -0,0 +1,16 @@
+// Conditional
+
+let result = true;
+let city = 'Dublin';
+let firstResponse;
+let secondResponse;
+
+if(result) {
+	firstResponse = 'result is true';
+}
+
+if(city) {
+	secondResponse = 'Thank you for choosing a city';
+} else {
+	secondResponse = 'You need to fill in the name of a City';
+}
