3ef020a0baf47444e41796fb7adfd896f1f9fdca
TaherCCG <taher.ccg@gmail.com>
2024-02-20 22:37:44 +0000

    add:Functions

---
+++ mt1-comparative-programming-languages/the-essentials/t5-abstracting-reusing-code/10a-calling-functions.py
@@ -0,0 +1,11 @@
+# Calling Functions in Python
+
+def say_hello():
+    print("Hello")
+
+say_hello() # prints Hello
+
+# The say_hello() function is defined with the def keyword. The function name is say_hello. The parentheses after the function name are used to pass arguments to the function. The colon at the end of the first line is used to indicate the start of the function body. The function body is indented by four spaces. The function body contains a single line of code that prints the string "Hello". The function is called using the say_hello() syntax. When the function is called, the code inside the function body is executed, and the string "Hello" is printed to the console.
+
+# The say_hello() function does not take any arguments. It simply prints the string "Hello" to the console. The function is called using the say_hello() syntax. When the function is called, the code inside the function body is executed, and the string "Hello" is printed to the console.
+
