2003f6b0c7d50d67ab5cc9faa715f920a5ec7d80
TaherCCG <taher.ccg@gmail.com>
2024-02-20 22:09:20 +0000

    fix:Folder structure - mt# = Module Topic - t# = Topic

---
+++ mt1-comparative-programming-languages/the-essentials/t5-abstracting-reusing-code/10-calling-functions.js
@@ -0,0 +1,25 @@
+// Calling Functions
+
+// Functions are called by using the function name followed by parentheses.
+// The parentheses may contain arguments separated by commas.
+
+function sayHello() {
+    console.log("Hello");
+}
+sayHello()
+
+function greet(name) {
+    console.log("Hello " + name);
+}
+greet("John")
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3))
+
+function subtract(a, b) {
+    return a - b;
+}   
+console.log(subtract(5, 3))
+
