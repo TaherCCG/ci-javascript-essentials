1b01d5007de9b99d6314065b1a5c96713bd74bad
TaherCCG <taher.ccg@gmail.com>
2024-02-22 11:31:58 +0000

    Add mouse scrolling functionality to the DOM

---
+++ m2- javascript-essentials/sm4-the-dom/smt3-handling-dom-events/t27-mouse-movements-and-scrolling/67-mouse-scrolling.js
@@ -0,0 +1,9 @@
+function turnPink() {
+    let color= document.getElementById('box');
+    color.style.backgroundColor="lightpink";
+}
+
+function turnYellow() {
+    let color= document.getElementById('box');
+    color.style.backgroundColor="yellow";
+}
