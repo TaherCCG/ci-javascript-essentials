b6bd010b2782d9ff1273b24a1587f436a7ae93dc
TaherCCG <taher.ccg@gmail.com>
2024-02-24 18:32:24 +0000

    Add DOM data structure code

---
+++ m2- javascript-essentials/sm4-the-dom/smt5-connecting-what-you-know/t38-dom-data-structure/77-dom-data-structure.js
@@ -0,0 +1,10 @@
+let ol = document.getElementsByTagName("ol")[0];
+let listItems =ol.children;
+
+let first =listItems[0].textContent;
+console.log(first);
+
+let fourth =listItems[3].textContent;
+console.log(fourth);
+
+
