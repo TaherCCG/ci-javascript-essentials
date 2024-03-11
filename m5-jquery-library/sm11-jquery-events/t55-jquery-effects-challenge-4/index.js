1212f5d89bdc4a29466322bf04fdcb176657690f
TaherCCG <taher.ccg@gmail.com>
2024-03-11 21:06:42 +0000

    Add index.css, index.html, and index.js files for jQuery effects challenge 4 fadeTo

---
+++ m5-jquery-library/sm11-jquery-events/t55-jquery-effects-challenge-4/index.js
@@ -0,0 +1,13 @@
+$('#button-card-1').mouseenter(function(){
+    $('#par-1').fadeTo(200,0.5);
+});
+$('#button-card-1').mouseleave(function(){
+    $('#par-1').fadeTo(200,1);
+});
+$('#button-card-2').mouseenter(function(){
+    $('#par-2').fadeTo(1000,0.5);
+});
+
+$('#button-card-2').mouseleave(function(){
+    $('#par-2').fadeTo(1000,1);
+});
