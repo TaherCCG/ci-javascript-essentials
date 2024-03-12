3681f2a0f9e1b195db02117c08b1f5c303afac0c
TaherCCG <taher.ccg@gmail.com>
2024-03-12 17:13:08 +0000

    Refactor main.js to display data in a table

--- m5-interactive-frontend-development/smt2-working-with-external-resources/main.js
+++ m5-interactive-frontend-development/smt2-working-with-external-resources/main.js
@@ -2,25 +2,45 @@ const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
 function getData(type, cb) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
 
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
 }
 
 function writeToDocument(type) {
-    var el=document.getElementById("data");
+    var el = document.getElementById("data");
     el.innerHTML = "";
-    getData(type, function (data) {
+
+    getData(type, function(data) {
+        var tableRows = [];
         data = data.results;
-        console.dir(data);
-        
-        data.forEach(function (item) {
-            el.innerHTML += "<p>" + item.name + "</p>";
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                dataRow.push(`<td>${item[key]}</td>`);
+            });
+            tableRows.push(dataRow)
         });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     });
 }
