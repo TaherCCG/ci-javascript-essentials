abd0a98ca082c53d2115e31990d5c33323234480
TaherCCG <taher.ccg@gmail.com>
2024-03-12 16:55:48 +0000

    Refactor writeToDocument function to display data as paragraphs

--- m5-interactive-frontend-development/smt2-working-with-external-resources/main.js
+++ m5-interactive-frontend-development/smt2-working-with-external-resources/main.js
@@ -13,7 +13,14 @@ function getData(type, cb) {
 }
 
 function writeToDocument(type) {
+    var el=document.getElementById("data");
+    el.innerHTML = "";
     getData(type, function (data) {
-        document.getElementById("data").innerHTML = data;
+        data = data.results;
+        console.dir(data);
+        
+        data.forEach(function (item) {
+            el.innerHTML += "<p>" + item.name + "</p>";
+        });
     });
 }
